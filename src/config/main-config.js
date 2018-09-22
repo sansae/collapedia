2dd1b9bb958e7ce13135c8759aed911e518bda9d
Kent <kentsaeteurn@gmail.com>
2018-09-22 03:22:45 -0700

    install express-flash and express-session to display flash messages

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -4,6 +4,9 @@ const viewsFolder = path.join(__dirname, "..", "views");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const expressValidator = require("express-validator");
+const session = require("express-session");
+const flash = require("express-flash");
+
 
 module.exports = {
   init(app, express){
@@ -12,8 +15,21 @@ module.exports = {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(expressValidator());
 
+    app.use(session({
+      secret: process.env.cookieSecret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 1.21e+9 }
+    }));
+
+    app.use((req, res, next) => {
+      res.locals.currentUser = req.user;
+      next();
+    });
+
+    app.use(flash());
     app.use(express.static(path.join(__dirname, "..", "assets")));
 
-    app.use(logger("dev"));
+    // app.use(logger("dev"));
   }
 };
