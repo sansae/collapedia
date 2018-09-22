cd2edf01067d1b46766d04f002f74f6312cd8bf7
Kent <kentsaeteurn@gmail.com>
2018-09-21 18:01:27 -0700

    set up controllers and views, install body-parser and ejs, and set view engine

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
 const logger = require("morgan");
 
 module.exports = {
   init(app, express){
+    app.set("views", viewsFolder);
+    app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use(express.static(path.join(__dirname, "..", "assets")));
+    
     app.use(logger("dev"));
   }
 };
