757f2c7df5dfd6104ac9620b61f1e70961c10b6a
Kent <kentsaeteurn@gmail.com>
2018-09-27 04:44:06 -0700

    create Wiki model, form association with User model

---
+++ src/db/models/wiki.js
@@ -0,0 +1,24 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Wiki = sequelize.define('Wiki', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    private: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    }
+  }, {});
+  Wiki.associate = function(models) {
+    Wiki.belongsTo(models.User, {
+      foreignKey: userId,
+      onDelete: "CASCADE"
+    });
+  };
+  return Wiki;
+};
