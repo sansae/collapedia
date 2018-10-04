543526e62b14f5b1ce867310e0403abc209dbe51
Kent <kentsaeteurn@gmail.com>
2018-10-04 01:00:42 -0700

    add sweetalert package to warn user about downgrade

---
+++ src/assets/js/sweetalert.js
@@ -0,0 +1,6 @@
+function sweetalert() {
+  swal({
+    type: 'info',
+    html: 'Please note that if you downgrade your account to Standard, any private wikis you have will become public.',
+  })
+}
